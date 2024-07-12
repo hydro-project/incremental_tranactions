7e7d80cbe9d331b2e9349c9b853925a5b847ce9b
Chris Douglas <chris_douglas@berkeley.edu>
2024-07-12 16:05:29 +0000

    fix byname query

--- incremental_transactions/tpcc/src/lib.rs
+++ incremental_transactions/tpcc/src/lib.rs
@@ -68,6 +68,122 @@ mod test {
 
     #[test]
     #[allow(unused_variables)]
+    fn test_byname_sql_inc() {
+        let cconf = CircuitConfig::with_workers(1);
+        let (mut circuit, handles) = byname_sql_incremental::circuit(cconf).unwrap();
+        let (
+            in_warehouse_static,
+            in_warehouse,
+            in_district_static,
+            in_district_next_id,
+            in_district_ytd,
+            in_customer,
+            in_transaction_parameters,
+            out_cust_agg,
+            out_cust_byname,
+        ) = handles;
+        let mut customers: Vec<_> = (0..5)
+            .into_iter()
+            .map(|i| {
+                (
+                    (
+                        Some(i),             // c_id
+                        Some(10),             // c_d_id
+                        Some(20),             // c_w_id
+                        Some(i.to_string()), // c_first
+                        None,
+                        Some("Public".to_string()), // c_last
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                    )
+                        .into(),
+                    1,
+                )
+                    .into()
+            })
+            .collect();
+        in_customer.append(&mut customers);
+
+        circuit.step().unwrap();
+        let agg = out_cust_agg.consolidate();
+        let byname = out_cust_byname.consolidate();
+
+        println!("Loaded customers (nothing to join, should be empty):");
+        byname
+            .iter()
+            .for_each(|(tup, _, z_weight)| println!("{:?}: {:?}", tup.0.unwrap(), z_weight));
+        // agg.iter().for_each(|(tup, _, z_weight)| {
+        //     println!(
+        //         "{:?}: {:?}",
+        //         tup.0
+        //             .iter()
+        //             .map(|x| match x {
+        //                 Some(value) => format!("{:?}, ", value),
+        //                 None => "_,".to_string(),
+        //             })
+        //             .collect::<String>(),
+        //         z_weight
+        //     )
+        // });
+
+        in_transaction_parameters.push(
+            (
+                Some(4344),                 // txn_id
+                None,                       // w_id
+                None,                       // d_id
+                None,                       // c_id
+                Some(20),                    // c_w_id
+                Some(10),                    // c_w_id
+                Some("Public".to_string()), // c_last
+                None,                       // h_amount
+                None,                       // h_date
+                None,                       // datetime_
+            )
+                .into(),
+            1,
+        );
+        circuit.step().unwrap();
+        let agg = out_cust_agg.consolidate(); // .merge(&agg);
+        let byname = out_cust_byname.consolidate(); // .merge(&byname);
+        println!("Result");
+        byname
+            .iter()
+            // .for_each(|(tup, _, z_weight)| println!("{:?}: {:?}", tup, z_weight));
+            .for_each(|(tup, _, z_weight)| println!("{:?} {:?}", tup, z_weight));
+            // .for_each(|(tup, _, z_weight)| println!("{:?} {:?} {:?} {:?} {:?} {:?}", tup.0.unwrap(), tup.2.unwrap(), tup.3.unwrap(), tup.4.unwrap(), tup.5.unwrap(), z_weight));
+        println!("cust_agg:");
+        agg.iter().for_each(|(tup, _, z_weight)| println!("{:?}: {:?}", tup.0.unwrap(), z_weight));
+        // println!("cust_agg:");
+        // agg.iter().for_each(|(tup, _, z_weight)| {
+        //     println!(
+        //         "{:?}: {:?}",
+        //         tup.0
+        //             .iter()
+        //             .map(|x| match x {
+        //                 Some(value) => format!("{:?}, ", value),
+        //                 None => "_,".to_string(),
+        //             })
+        //             .collect::<String>(),
+        //         z_weight
+        //     )
+        // });
+    }
+
+    #[test]
+    #[allow(unused_variables)]
     fn test_byname_sql() {
         let cconf = CircuitConfig::with_workers(1);
         let (mut circuit, handles) = byname_sql::circuit(cconf).unwrap();
@@ -117,28 +233,28 @@ mod test {
             .collect();
         in_customer.append(&mut customers);
 
-        circuit.step().unwrap();
-        let byname = out_cust_byname.consolidate();
-        let agg = out_cust_agg.consolidate();
+        // circuit.step().unwrap();
+        // let byname = out_cust_byname.consolidate();
+        // let agg = out_cust_agg.consolidate();
 
-        println!("Loaded customers (nothing to join, should be empty):");
-        byname
-            .iter()
-            .for_each(|(tup, _, z_weight)| println!("{:?}: {:?}", tup.0.unwrap(), z_weight));
-        println!("cust_agg:");
-        agg.iter().for_each(|(tup, _, z_weight)| {
-            println!(
-                "{:?}: {:?}",
-                tup.0
-                    .iter()
-                    .map(|x| match x {
-                        Some(value) => format!("{:?}, ", value),
-                        None => "_,".to_string(),
-                    })
-                    .collect::<String>(),
-                z_weight
-            )
-        });
+        // println!("Loaded customers (nothing to join, should be empty):");
+        // byname
+        //     .iter()
+        //     .for_each(|(tup, _, z_weight)| println!("{:?}: {:?}", tup.0.unwrap(), z_weight));
+        // println!("cust_agg:");
+        // agg.iter().for_each(|(tup, _, z_weight)| {
+        //     println!(
+        //         "{:?}: {:?}",
+        //         tup.0
+        //             .iter()
+        //             .map(|x| match x {
+        //                 Some(value) => format!("{:?}, ", value),
+        //                 None => "_,".to_string(),
+        //             })
+        //             .collect::<String>(),
+        //         z_weight
+        //     )
+        // });
 
         in_transaction_parameters.push(
             (
@@ -157,25 +273,25 @@ mod test {
             1,
         );
         circuit.step().unwrap();
-        let byname = out_cust_byname.consolidate().merge(&byname);
-        let agg = out_cust_agg.consolidate().merge(&agg);
+        let byname = out_cust_byname.consolidate(); // .merge(&byname);
+        let agg = out_cust_agg.consolidate(); // .merge(&agg);
         println!("Result");
         byname
             .iter()
             .for_each(|(tup, _, z_weight)| println!("{:?}: {:?}", tup.0.unwrap(), z_weight));
-        println!("cust_agg:");
-        agg.iter().for_each(|(tup, _, z_weight)| {
-            println!(
-                "{:?}: {:?}",
-                tup.0
-                    .iter()
-                    .map(|x| match x {
-                        Some(value) => format!("{:?}, ", value),
-                        None => "_,".to_string(),
-                    })
-                    .collect::<String>(),
-                z_weight
-            )
-        });
+        // println!("cust_agg:");
+        // agg.iter().for_each(|(tup, _, z_weight)| {
+        //     println!(
+        //         "{:?}: {:?}",
+        //         tup.0
+        //             .iter()
+        //             .map(|x| match x {
+        //                 Some(value) => format!("{:?}, ", value),
+        //                 None => "_,".to_string(),
+        //             })
+        //             .collect::<String>(),
+        //         z_weight
+        //     )
+        // });
     }
 }
